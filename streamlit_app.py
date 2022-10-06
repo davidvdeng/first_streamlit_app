331619bf1b0a299af677c40b5d6a501b92cc0fc5
davidvdeng <110893406+davidvdeng@users.noreply.github.com>
2022-10-06 15:21:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ streamlit.caption('Omega 3 and Blueberry Oatmeal')
 
 streamlit.caption('Kale, Spinach and Rocket Smoothie')
 
-streamlit.caption('Hard-Boiled Free Range Eggs')
+streamlit.caption('Hard-Boiled Free Range Egg')
