29335199011b4d009b831bc116b395a78ba163a0
davidvdeng <110893406+davidvdeng@users.noreply.github.com>
2022-10-06 15:35:27 +0200

    Update streamlit_app.py
    error fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free Range Egg')
-streamlit.text('🥑🍞Avocado Toast
+streamlit.text('🥑🍞Avocado Toast')
